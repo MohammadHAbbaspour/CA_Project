041fa7049c733a2310686fba6d6181f5faa9a759
Mohammad Osoolian <mohammad_osoolian@comp.iust.ac.ir>
2022-07-08 16:49:19 +0430

    cache finished

--- ca_project/Storage/src/my pachage.vhd
+++ ca_project/Storage/src/my pachage.vhd
@@ -7,5 +7,8 @@ package my_package is
 	type two_word_data_Type is array (0 to 1) of std_logic_vector(31 downto 0);	
 	type page_type is array (0 to 15) of two_word_data_type;	
 	type memory_data_blocks_type is array (0 to 12) of page_type;
+	type cach_data_blocks_type is array (0 to 31) of two_word_data_Type;
+	type tag_data_type is array (0 to 31) of std_logic_vector(2 downto 0);
 end my_package;
 
+				
